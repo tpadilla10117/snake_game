a22be47b448b4ef1db008083ff35e2fc65693bad
Trinidad Padilla <tpadilla101@live.com>
2021-11-21 11:48:57 -0800

    got rid of uneccesary files

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,46 +1,17 @@
 /* This components handles exports in the main App: */
 
 // Layout:
-export { default as Navbar } from './layout/navbar/Navbar.jsx';
+/* export { default as Navbar } from './layout/navbar/Navbar.jsx'; */
 
-export { default as Sidebar } from './layout/sidebar/Sidebar.jsx'; 
 
-export { default as Header } from './layout/header/Header.jsx';
-
-export { default as EmailForm } from './layout/emailform/EmailForm.jsx';
-
-export { default as SectionWrapper } from './layout/sectionwrapper/SectionWrapper.jsx';
-
-export { default as Modal } from './layout/modal/Modal.jsx';
-
-export { default as CardWrapper } from './layout/cardwrapper/CardWrapper.jsx';
-
-export { default as ProjectCard } from './layout/projectcard/ProjectCard.jsx';
-
-export { default as ProjectPage } from './layout/projectpage/ProjectPage.jsx';
-
-export { default as BulletList } from './layout/bulletlist/BulletList.jsx';
-
-export { default as LinkBlock } from './layout/linkblock/LinkBlock.jsx';
-
-export { default as LinkButton } from './layout/linkbutton/LinkButton.jsx';
 
 
 /* -------------------------------------------------------------------------- */
 
 //Pages:
 
-export { default as Landing } from './pages/landing/Landing.jsx';
+/* export { default as Landing } from './pages/landing/Landing.jsx'; */
 
-export { default as Developer } from './pages/developer/Developer.jsx';
-
-export { default as AboutMe } from './pages/about/AboutMe.jsx';
-
-export { default as Ux } from './pages/ux/UX.jsx';
-
-export { default as Contact } from './pages/contact/Contact.jsx';
-
-export { default as Resume } from './pages/resume/Resume.jsx';
 
 
 /* -------------------------------------------------------------------------- */
