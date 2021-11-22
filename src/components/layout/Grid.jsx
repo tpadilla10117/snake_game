497950253845d2cb4191f490544183ec2aa5784d
Trinidad Padilla <tpadilla101@live.com>
2021-11-22 11:32:20 -0800

    created an update and draw function

--- src/components/layout/Grid.jsx
+++ src/components/layout/Grid.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 import './Grid.styles.scss';
+import { SNAKE_SPEED, draw as drawSnake, update as updateSnake } from './Snake';
 
 function Grid() {
 
     let lastRenderTime = 0;
 
-    /* Game Loop: */
+/* How many times the snake moves per second:  */
+    /* const SNAKE_SPEED = 2; */
+
+/* Game Loop: */
     function main(currentTime) {
+        window.requestAnimationFrame(main);/* browser, tell me when to I can render my next frame.  I start with requesting a frame to animate my game, then I get the timestamp back when that frame is going to render*/
+
         const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000; /* convert to second from milliseconds */
 
-        window.requestAnimationFrame(main);/* browser, tell me when to I can render my next frame.  I start with requesting a frame to animaye my game, then I get the timestamp back when that frame is going to render*/
+    /* If the seconds from our last render is less than the time between our renders  */
+        if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+        /* console.log("Here is the current time: ", currentTime); */
+        console.log("Render: ");
 
         lastRenderTime = currentTime;
-        /* console.log("Here is the current time: ", currentTime); */
-        console.log(secondsSinceLastRender);
+        update();
+        draw();
     }
 
+
     /* console.log(window.requestAnimationFrame(main)); */
+    /* window.requestAnimationFrame(main); */
+
+    function update() {
+        updateSnake();
+    }
+
+    function draw() {
+        drawSnake();
+    }
 
     return (
         <div id="game-board">
---
+++ src/components/layout/Snake.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+/* How many times the snake moves per second:  */
+export const SNAKE_SPEED = 2;
+
+/* Update loop - updates all of the logic for your game : */
+export function update() {
+    console.log("update snake");
+}
+
+/* Draw / Render Loop - what draws everything on the screen based on the update loop */
+export function draw() {
+    console.log("draw snake");
+}
